5d94a03e92f6077aa2aa0257a0eddcbe04e6d193
helpful-database-6850 <antwaun.l.johnson@gmail.com>
2019-05-29 21:37:08 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -30,6 +30,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 
 function removeElementFromBeginningOfArray(array){
   return array.slice(1)
+  array
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
